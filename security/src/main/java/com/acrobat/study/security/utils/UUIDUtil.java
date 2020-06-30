6d85d401bcbf30c3802e103605cd1be92f1e0b54
xutao <904236131@qq.com>
2020-06-30 17:51:24 +0800

    test

---
+++ security/src/main/java/com/acrobat/study/security/utils/UUIDUtil.java
@@ -0,0 +1,16 @@
+package com.acrobat.study.security.utils;
+
+import java.util.UUID;
+
+/**
+ * UUID生成工具类
+ */
+public class UUIDUtil {
+
+    /**
+     * 生成唯一UUID
+     */
+    public static String uuid() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+}
