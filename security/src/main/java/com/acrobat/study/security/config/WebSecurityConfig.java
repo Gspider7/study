e4b0f06f585b3270bebce0ece2c91ebe3451ba40
xutao <904236131@qq.com>
2020-07-01 17:29:38 +0800

    test

--- security/src/main/java/com/acrobat/study/security/config/WebSecurityConfig.java
+++ security/src/main/java/com/acrobat/study/security/config/WebSecurityConfig.java
@@ -24,12 +24,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
-    @Value("server.servlet.context-path")
+    @Value("${server.servlet.context-path}")
     private String contextPath;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // 这里密码没有使用编码
+        // 这里密码没有使用编码，如果需要使用，数据库中存储的密码必须使用同样的方式进行编码才能匹配
         auth.userDetailsService(userDetailsService).passwordEncoder(new PasswordEncoder() {
             @Override
             public String encode(CharSequence charSequence) {
@@ -43,24 +43,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         });
     }
 
+    /**
+     * 注意，这里配置的所有地址都默认带上了contextPath
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();                     // 关闭CSRF跨域
+        // 解决h2控制台空白问题，参考：https://www.jianshu.com/p/925d5aece6dc
+        http.csrf().disable();                                  // 关闭CSRF攻击防御
+        http.headers().frameOptions().sameOrigin();
 
         http.authorizeRequests()
-                // 跨域预检请求
-                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()     // 跨域预检请求
 //                .antMatchers("/static/**").permitAll()        // 静态资源允许匿名访问
                 .antMatchers("/user/**").permitAll()
+                .antMatchers("/h2/**").hasRole("ADMIN")         // 注意这里配置角色需要去掉ROLE_前缀
                 .anyRequest().authenticated();                  // 其他所有请求需要身份认证
 
         // 引入内置登录接口，接口地址为/login，注意这里配置的是登录页的地址
-        http.formLogin().loginPage(contextPath + "/user/login")
-                .defaultSuccessUrl(contextPath + "/user/home")
-                .failureForwardUrl(contextPath + "/user/login");
-                // 自定义登录用户名和密码参数，默认为username和password
-//                .usernameParameter("username")
-//                .passwordParameter("password")
+        http.formLogin() // 表单登录
+                // http.httpBasic() // HTTP Basic
+                .loginPage("/user/login")                       // 跳转登录页的地址
+                .loginProcessingUrl("/login")                   // 登录表单请求的地址，不需要写这个地址的接口，由spring security处理
+                .successForwardUrl("/user/home");               // 登录成功跳转地址
 
         // 引入内置登出接口
         http.logout().clearAuthentication(true)
@@ -72,4 +76,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // 静态资源等放行
         web.ignoring().antMatchers("/static/**", "/other/**");
     }
+
+    // ----------------------------------------------------------------------
 }
