ce2ffc16913e6a96f1efe9de453fcabf0462b431
xutao <904236131@qq.com>
2020-07-07 11:55:42 +0800

    test

---
+++ security/src/main/java/com/acrobat/study/security/service/SysUserService.java
@@ -0,0 +1,14 @@
+package com.acrobat.study.security.service;
+
+import com.acrobat.study.security.entity.SysUser;
+import com.acrobat.study.security.mapper.SysUserMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * mybatis plus的service实现了一些基本方法
+ */
+@Service
+public class SysUserService extends ServiceImpl<SysUserMapper, SysUser> {
+
+}
--- security/src/test/java/com/acrobat/study/security/MybatisTest.java
+++ security/src/test/java/com/acrobat/study/security/MybatisTest.java
@@ -2,6 +2,7 @@ package com.acrobat.study.security;
 
 import com.acrobat.study.security.entity.SysUser;
 import com.acrobat.study.security.mapper.SysUserMapper;
+import com.acrobat.study.security.service.SysUserService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,8 @@ public class MybatisTest {
 
     @Autowired
     private SysUserMapper sysUserMapper;
+    @Autowired
+    private SysUserService sysUserService;
 
     @Test
     public void test() {
@@ -30,8 +33,10 @@ public class MybatisTest {
 
         // QueryWrapper用来组装where条件
         QueryWrapper<SysUser> wrapper = new QueryWrapper<>();
-        wrapper.eq("username", "test");
+        wrapper.eq("username", "test").select("username", "password");
         SysUser user3 = sysUserMapper.selectOne(wrapper);
+
+//        sysUserService.saveOrUpdateBatch(..);
 
         System.out.println();
     }
