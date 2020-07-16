c45357feb863cdce46d1407afc068a20a5c78c17
xutao <904236131@qq.com>
2020-07-16 19:18:21 +0800

    test

--- security/src/main/java/com/acrobat/study/security/controller/TestController.java
+++ security/src/main/java/com/acrobat/study/security/controller/TestController.java
@@ -3,7 +3,11 @@ package com.acrobat.study.security.controller;
 import com.acrobat.study.security.utils.JacksonUtil;
 import com.acrobat.study.security.vo.TestVO;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -23,5 +27,13 @@ public class TestController {
     public String testValidation(@Valid @RequestBody TestVO testVO) throws JsonProcessingException {
 
         return JacksonUtil.writeValueAsString(testVO);
+    }
+
+    @ResponseBody
+    @RequestMapping("/permitAll")
+    @PreAuthorize("permitAll")
+    public String testPermitAll() {
+
+        return "permitAll允许任何用户访问，但用户需要登录";
     }
 }
