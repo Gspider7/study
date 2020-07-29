cb0f225f554f2918d79ab6f9ec8409cd8bbb7f2b
xutao <904236131@qq.com>
2020-07-29 10:10:16 +0800

    test

---
+++ security/src/main/java/com/acrobat/study/security/feign/FeignAuthInterceptor.java
@@ -0,0 +1,37 @@
+package com.acrobat.study.security.feign;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * Feign请求拦截器（设置请求头，传递登录信息）
+ * Feign是重新构建请求调用，默认不会继承用户请求中的信息
+ **/
+@Component
+public class FeignAuthInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            Enumeration<String> headerNames = request.getHeaderNames();
+            if (headerNames != null) {
+                while (headerNames.hasMoreElements()) {
+                    String name = headerNames.nextElement();
+                    String values = request.getHeader(name);
+                    requestTemplate.header(name, values);
+                }
+            }
+        }
+
+    }
+
+}
